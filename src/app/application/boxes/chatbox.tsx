fb58e22d2a248f54beaa4215e7a0f13adb632e24
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-03-09 15:49:26 +0800

    ok

--- src/app/application/boxes/chatbox.tsx
+++ src/app/application/boxes/chatbox.tsx
@@ -1,37 +1,38 @@
 import React from 'react';
 
 const Chatbox: React.FC = () => {
-  // Example list of people
+
   const people: string[] = ['Alex', 'Amanda', 'Jenny', 'John', 'Sarah'];
 
   return (
-    <div className="h-[63vh] w-[20vw] rounded-xl border border-zinc-400 bg-zinc-200 mr-5 p-4 flex flex-col justify-center items-center shadow-lg overflow-y-auto">
 
-<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+<div className="mr-2 h-[63vh] w-[20vw] border border-zinc-400 rounded-lg shadow-lg bg-zinc-200">
+<table className="">
+  <thead className="text-xs text-gray-700 uppercase dark:bg-gray-700 dark:text-gray-400">
     <tr>
-      <th scope="col" className="px-6 py-3">
+      <th scope="col" className="px-6 py-6">
         Chats
       </th>
     </tr>
   </thead>
   <tbody>
-    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-      <td className="flex flex-col px-6 py-4 font-medium text-gray-900 dark:text-white cursor-pointer">
+    <tr className="">
+      <td className="flex flex-col px-2 py-1 font-medium text-gray-900 cursor-pointer">
         <div className="mb-2">Joshua No Amores</div>
         <div className="mb-2">Phil Baby Diaz</div>
         <div className="mb-2">Dragon Ronel Beriana</div>
         <div className="mb-2">This guy will be back</div>
-        <div>Status: No Progress</div>
       </td>
     </tr>
   </tbody>
 </table>
+</div>
 
 
 
 
-    </div>
+
+ 
   );
 };
 
