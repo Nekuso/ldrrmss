d2ed3457176b8c5ec7a2f1081d31d5b8885678d2
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-03-09 19:39:36 +0800

    help nixo

--- src/app/application/announcement/page.tsx
+++ src/app/application/announcement/page.tsx
@@ -2,12 +2,59 @@ import React from 'react';
 
 const Announcement: React.FC = () => {
   return (
-    <div className="w-full h-full">
-        <h1 className="text-black font-bold ml-4 mb-2 ">Announcement</h1>
-<div className="w-[94%] h-[94%] rounded-xl border border-white bg-white mr-7 shadow-2xl">
+    <div className="w-full h-full flex flex-col items-center">
+      <h1 className="text-black font-bold ml-4 mb-2">Announcement</h1>
+      <div className="w-[94%] h-[94%] rounded-xl border border-white bg-white mr-7 shadow-2xl">
+
+      <button type="button" className=" bg-teal-100 border-2 border-teal-500 text-teal-900 flex items-center justify-center focus:ring-1 focus:ring-primary-300 rounded-lg text-sm px-4 py-2 m-2 h-[7vh] w-[16vw]">
+        <svg
+                      aria-hidden="true"
+                      className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"/>
+        </svg>
+            <span className="text-emerald-900">Create Announcement</span>
+      </button>
 
 
-</div></div>
-);
+
+
+        <div className="bg-teal-100 border-t-4 border-teal-500 rounded-lg text-teal-900 px-1 py-3 shadow-md mt-2 ml-2 w-[90%]" role="alert">
+          <div className="flex items-center">
+            <div className="py-1">
+              <svg className="fill-current h-6 w-6 text-teal-500 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/>
+              </svg>
+            </div>
+            <div>
+              <p className="font-bold">New Memorandum</p>
+              <p className="text-sm">Make sure you know how these changes affect you.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-orange-200 border-t-4 border-orange-700 rounded-lg text-teal-900 px-1 py-3 shadow-md mt-2 ml-2 w-[90%]" role="alert">
+          <div className="flex items-center">
+            <div className="py-1">
+              <svg className="fill-current h-6 w-6 text-orange-500 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/>
+              </svg>
+            </div>
+            <div>
+              <p className="font-bold">New Rescue tips </p>
+              <p className="text-sm">Make sure you know how these changes affect you.</p>
+            </div>
+          </div>
+        </div>
+
+      </div> </div>
+
+  );
 }
+
 export default Announcement;
--- src/app/application/stocks/page.tsx
+++ src/app/application/stocks/page.tsx
@@ -1,13 +1,73 @@
 import React from 'react';
 
 const Stocks: React.FC = () => {
+  const product = {
+    name: 'Jasmine Rice (Per kilo)',
+    category: 'Food',
+    amount: '10',
+    date: '02/25/2024',
+  };
+
   return (
     <div className="w-full h-full">
-        <h1 className="text-black font-bold ml-4 mb-2 ">Stocks</h1>
-<div className="w-[94%] h-[94%] rounded-xl border border-white bg-white mr-7 shadow-2xl">
+      <h1 className="text-black font-bold ml-4 mb-2">Stocks</h1>
+      <div className="w-[94%] h-[94%] rounded-xl border border-white bg-white mr-7 shadow-2xl">
+        <div className="overflow-x-auto">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-4 py-3">
+                  Name
+                </th>
+                <th scope="col" className="px-4 py-3">
+                  Request Type
+                </th>
+                <th scope="col" className="px-4 py-3">
+                  Request Time & Date
+                </th>
+                <th scope="col" className="px-4 py-3">
+                  Status
+                </th>
+                <th scope="col" className="px-4 py-3">
+                  <span className="sr-only text-gray-500">Actions</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="px-4 py-3">{product.name}</td>
+                <td className="px-4 py-3">{product.category}</td>
+                <td className="px-4 py-3">{product.amount}</td>
+                <td className="px-4 py-3">{product.date}</td>
+                <td className="px-4 py-3 relative group">
+                  <button
+                    type="button"
+                    className="flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                  >
+                    <svg
+                      aria-hidden="true"
+                      className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                      />
+                    </svg>
+                    <span className="text-gray-500">Add product</span>
+                  </button>
 
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-</div></div>
-);
-}
 export default Stocks;
