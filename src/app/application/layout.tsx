493671910730d8d276c8eb1d429326d70a359f96
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-03-29 13:26:14 +0800

    adad

--- src/app/application/layout.tsx
+++ src/app/application/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
       <body className={montserrat.className}>
         <div className="flex place-items-center justify-center w-full h-screen from-slate-200 to-slate-500">
           <div className="w-full h-full bg-white flex flex-col shadow-2xl">
-            <div className="flex flex-col w-full h-full bg-slate-200">
+            <div className="flex flex-col w-full h-full bg-slate-200 ">
               <div className="flex w-full h-fit justify-center place-items-center border-b items-center px-4 py-4 bg-white">
                 <div className="w-[90%] flex justify-between place-items-center">
                   <MainNav className="mx-6" />
