4d443bfb266b1ef64d8495af07d35177b8fb174c
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-03-31 12:45:27 +0800

    designing the standin

--- src/app/application/standin/page.tsx
+++ src/app/application/standin/page.tsx
@@ -4,12 +4,85 @@ export default function Page() {
     <div className="flex flex-col justify-start place-items-center w-full h-full gap-7 p-8">
       <div className="space-y-2 w-[90%] h-fit bg-white p-4 rounded-2xl">
         <div className="w-full h-full flex max-w-[1840px] max-h-[900px] justify-center place-items-center gap-7">
-          <div className="w-full h-full flex flex-col gap-7 rounded-xl">
-            <div className="w-full h-full flex flex-col gap-2 justify-around place-items-center bg-darkComponentBg rounded-xl shadow-lg border border-lightBorder overflow-hidden p-6"></div>
-          </div>
-          <div className="w-[35%] 2xl:w-[30%] h-full flex flex-col gap-7">
-            <div className="w-full h-full flex flex-col gap-2 justify-around place-items-center bg-darkComponentBg rounded-xl shadow-lg border border-lightBorder overflow-hidden p-6"></div>
-            <div className="w-full h-full flex flex-col gap-2 justify-around place-items-center bg-darkComponentBg rounded-xl shadow-lg border border-lightBorder overflow-hidden p-6"></div>
+          <div className="w-full h-full flex gap-7 rounded-xl">
+            <div className="w-full h-full flex flex-col gap-2 justify-around place-items-center bg-darkComponentBg rounded-xl shadow-lg border border-lightBorder overflow-hidden p-6">
+              <div className="w-full flex flex-row gap-7">
+                <div className="w-full flex flex-col gap-2">
+                  <span className="text-xs 2xl:text-md font-semibold text-gray-400 flex justify-center place-items-center w-fit gap-1">
+                    Name
+                  </span>
+                  <div className="w-full min-w-0 bg-gray-700 rounded-lg">
+                    <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                      Terrencepaul
+                    </p>
+                  </div>
+                </div>
+
+                <div className="w-full flex flex-col gap-2">
+                  <span className="text-xs 2xl:text-md font-semibold text-gray-400 flex justify-center place-items-center w-fit gap-1">
+                    Name
+                  </span>
+                  <div className="w-full min-w-0 bg-gray-700 rounded-lg">
+                    <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                      Terrencepaul
+                    </p>
+                  </div>
+                </div>
+
+                <div className="w-full flex flex-col gap-2">
+                  <span className="text-xs 2xl:text-md font-semibold text-gray-400 flex justify-center place-items-center w-fit gap-1">
+                    Name
+                  </span>
+                  <div className="w-full min-w-0 bg-gray-700 rounded-lg">
+                    <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                      Terrencepaul
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full flex flex-row gap-7">
+                <div className="w-full flex flex-col gap-2">
+                  <span className="text-xs 2xl:text-md font-semibold text-gray-400 flex justify-center place-items-center w-fit gap-1">
+                    Name
+                  </span>
+                  <div className="w-full min-w-0 bg-gray-700 rounded-lg">
+                    <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                      Terrencepaul
+                    </p>
+                  </div>
+                </div>
+
+                <div className="w-full flex flex-col gap-2">
+                  <span className="text-xs 2xl:text-md font-semibold text-gray-400 flex justify-center place-items-center w-fit gap-1">
+                    Name
+                  </span>
+                  <div className="w-full min-w-0 bg-gray-700 rounded-lg">
+                    <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                      Terrencepaul
+                    </p>
+                  </div>
+                </div>
+
+                <div className="w-full flex flex-col gap-2">
+                  <span className="text-xs 2xl:text-md font-semibold text-gray-400 flex justify-center place-items-center w-fit gap-1">
+                    Name
+                  </span>
+                  <div className="w-full min-w-0 bg-gray-700 rounded-lg">
+                    <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                      Terrencepaul
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-[35%] 2xl:w-[30%] h-full flex flex-col gap-7">
+              <div className="w-full h-full flex flex-col gap-2 justify-around place-items-center bg-darkComponentBg rounded-xl shadow-lg border border-lightBorder overflow-hidden p-6">
+                Profile
+              </div>
+              <div className="w-full h-full flex flex-col gap-2 justify-around place-items-center bg-darkComponentBg rounded-xl shadow-lg border border-lightBorder overflow-hidden p-6">
+                Map Location
+              </div>
+            </div>
           </div>
         </div>
       </div>
