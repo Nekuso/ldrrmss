ba823dc372f329b77a976491bb0e7741509bbb48
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-03-31 21:35:12 +0800

    For now

--- src/app/application/standin/page.tsx
+++ src/app/application/standin/page.tsx
@@ -120,33 +120,39 @@ export default function Page() {
                 <Button>Delete</Button>
               </div>
             </div>
-            <div className="w-[35%] 2xl:w-[30%] h-fit flex flex-col gap-7">
+            <div className="w-[50%] 2xl:w-[50%] h-fit flex flex-col gap-2">
               <div className="w-full h-fit flex flex-col gap-2 justify-around bg-white place-items-center bg-darkComponentBg rounded-2xl shadow-lg border border-lightBorder overflow-hidden">
                 <div className="w-full h-[170px] 2xl:h-[210px] bg-black rounded-xl relative">
                   <img
                     alt="picture"
-                    className="w-full h-full object-cover rounded-xl opacity-75"
+                    className="w-full h-full object-cover opacity-75"
                     src="https://storage.ko-fi.com/cdn/useruploads/display/6bc95a9e-2521-4933-b482-e5574f92c833_8-final.jpg"
                   ></img>
-                  <span className="flex shrink-0 overflow-hidden rounded-full w-40 h-40 cursor-pointer z-0 absolute -bottom-[30%] left-7 border-8 border-darkComponentBg">
-                    <span className="flex h-full w-full items-center justify-center rounded-full bg-lightComponentBg font-bold text-2xl">
-                      S M
+                  <span className="flex overflow-hidden rounded-full w-40 h-40 cursor-pointer z-0 absolute -bottom-[35%] left-40 border-8 border-neutral-900 bg-slate-500 ">
+                    <span className="flex h-full w-full items-center justify-center rounded-full font-bold text-2xl text-white">
+                      TD
                     </span>
                   </span>
                 </div>
-                <div className="w-full h-full flex justify-start gap-4">
-                  <h2 className="flex text-2xl font-bold place-items-center gap-3">
+                <div className="w-full h-full flex justify-center py-20">
+                  <h2 className="flex text-2xl font-bold place-items-center gap-3 text-center">
                     Terrence Dante
                     <div className="text-xs rounded-full py-1 px-2 border font-normal flex place-items-center gap-1 cursor-pointer text-red-500 bg-red-500 bg-opacity-20 border-red-500">
-                      <div className="w-2 h-2 rounded-full bg-red-500">
-                        "Unavailable"
-                      </div>
+                      <div className="w-2 h-2 rounded-full bg-red-500"></div>
+                      Unavailable
                     </div>
                   </h2>
                 </div>
               </div>
-              <div className="w-full h-full flex flex-col gap-2 justify-around bg-white place-items-center bg-darkComponentBg rounded-2xl shadow-lg border border-lightBorder overflow-hidden p-6">
-                Map Location
+              <div className="w-full h-full flex flex-col gap-2 justify-around bg-white place-items-center bg-darkComponentBg rounded-2xl shadow-lg border border-lightBorder overflow-hidden">
+                <div className="w-full h-full">
+                  <iframe
+                    className="w-full h-full"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.644211999011!2d123.2626467!3d9.289425199999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zOsKwMjMnMzQuMCJTIDEyM8KwMzMnMTAuNyJF!5e0!3m2!1sen!2sph!4v1648767287790!5m2!1sen!2sph"
+                    allowFullScreen={true}
+                    loading="lazy"
+                  ></iframe>
+                </div>
               </div>
             </div>
           </div>
