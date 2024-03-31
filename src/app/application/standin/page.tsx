a30c845a5ae1e1896c4db1fb90189835279b398c
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-03-31 13:48:02 +0800

    refined standin

--- src/app/application/standin/page.tsx
+++ src/app/application/standin/page.tsx
@@ -3,12 +3,13 @@ import React from "react";
 export default function Page() {
   return (
     <div className="flex flex-col justify-start place-items-center w-full h-full gap-7 p-8">
-      <div className="space-y-2 w-[90%] h-fit bg-white p-4 rounded-2xl">
+      <div className="space-y-2 w-[90%] h-fit rounded-2xl">
         <div className="w-full h-full flex max-w-[1840px] max-h-[900px] justify-center place-items-center gap-7">
           <div className="w-full h-full flex gap-7 rounded-xl">
-            <div className="w-full h-full flex flex-col gap-2 justify-around bg-darkComponentBg rounded-xl shadow-lg border border-lightBorder overflow-hidden p-6">
-              <h1>General Information</h1>
-
+            <div className="w-full h-full flex flex-col gap-2 justify-around bg-white bg-darkComponentBg rounded-2xl shadow-lg border border-lightBorder overflow-hidden p-6">
+              <div className="font-bold py-1">
+                <h1>General Information</h1>
+              </div>
               <div className="w-full flex flex-row gap-7">
                 <div className="w-full flex flex-col gap-2">
                   <span className="text-xs 2xl:text-md font-semibold text-gray-400 flex justify-center place-items-center w-fit gap-1">
@@ -77,7 +78,9 @@ export default function Page() {
                   </div>
                 </div>
               </div>
-              <h1>Worker Information</h1>
+              <div className="font-bold py-4">
+                <h1>Worker Information</h1>
+              </div>
               <div className="w-full flex flex-row gap-7">
                 <div className="w-full flex flex-col gap-2">
                   <span className="text-xs 2xl:text-md font-semibold text-gray-400 flex justify-center place-items-center w-fit gap-1">
@@ -112,16 +115,16 @@ export default function Page() {
                   </div>
                 </div>
               </div>
-              <div className="justify-center text-center w-full space-x-1">
+              <div className="justify-center text-center w-full space-x-1 space-y-4">
                 <Button>Update</Button>
                 <Button>Delete</Button>
               </div>
             </div>
             <div className="w-[35%] 2xl:w-[30%] h-full flex flex-col gap-7">
-              <div className="w-full h-full flex flex-col gap-2 justify-around place-items-center bg-darkComponentBg rounded-xl shadow-lg border border-lightBorder overflow-hidden p-6">
+              <div className="w-full h-full flex flex-col gap-2 justify-around bg-white place-items-center bg-darkComponentBg rounded-2xl shadow-lg border border-lightBorder overflow-hidden p-6">
                 Profile
               </div>
-              <div className="w-full h-full flex flex-col gap-2 justify-around place-items-center bg-darkComponentBg rounded-xl shadow-lg border border-lightBorder overflow-hidden p-6">
+              <div className="w-full h-full flex flex-col gap-2 justify-around bg-white place-items-center bg-darkComponentBg rounded-2xl shadow-lg border border-lightBorder overflow-hidden p-6">
                 Map Location
               </div>
             </div>
