e61f80ef09b33f2b9d8b0b489ce24462a094fec5
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-04-03 00:10:30 +0800

    gitpush

--- src/app/application/requests/createrequest/create-request-form.tsx
+++ src/app/application/requests/createrequest/create-request-form.tsx
@@ -1,3 +1,6 @@
+import { Input } from "@/components/ui/input";
+import { table } from "console";
+
 export function Createrequestform() {
   return (
     <>
@@ -19,8 +22,19 @@ export function Createrequestform() {
               className="flex h-10 w-full border px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 rounded-lg bg-lightComponentBg border-slate-600/50"
               placeholder="Enter Last Name"
             ></input>
-            <div className="form-message"></div>
           </div>
+        </div>
+      </div>
+      <div className="w-full flex flex-col">
+        <div className="form-item">
+          <label className="form-label text-xs font-medium">
+            Search Account
+          </label>
+          <Input
+            placeholder="Search account ..."
+            className="flex h-10 w-full border px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 rounded-lg bg-lightComponentBg border-slate-600/50"
+          />
+          <div className="form-message"></div>
         </div>
       </div>
     </>
