40ba94671bc327209e5f278a25f9917194952027
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-04-06 17:18:59 +0800

    Polishing the sub user-profile

--- src/app/application/employees/user/[id]/user-content.tsx
+++ src/app/application/employees/user/[id]/user-content.tsx
@@ -183,27 +183,28 @@ export default function Page({ employee, roles }: any) {
             </div>
           </div>
         </div>
+        {/*Bunda rhama*/}
 
-        <div className="relative flex flex-col flex-auto min-w-0 p-4 overflow-hidden break-words border-0 shadow-blur rounded-2xl bg-white bg-clip-border draggable">
+        <div className="relative flex flex-col flex-auto min-w-0 p-4 overflow-hidden break-words border-0 shadow-blur rounded-2xl bg-gradient-to-r from-blue-300 via-blue-400 to-blue-100 bg-clip-border draggable shadow-lg">
           <div className="flex flex-wrap -mx-3">
             <div className="flex-none w-auto max-w-full px-3">
               <div className="text-base ease-soft-in-out h-18.5 w-18.5 relative inline-flex items-center justify-center rounded-xl text-white transition-all duration-200">
-                <img
-                  src="https://static1.personality-database.com/profile_images/a245f04f64d34da29e920e0547a7d4e7.png"
-                  alt="profile_image"
-                  className="max-w-full shadow-soft-sm rounded-3xl max-h-20"
-                />
+                <span className="flex rounded-3xl w-20 h-20 cursor-pointer border-4 border-neutral-900 bg-slate-500 ">
+                  <span className="flex h-full w-full items-center justify-center font-bold text-2xl text-white">
+                    TD
+                  </span>
+                </span>
               </div>
             </div>
             <div className="flex-none w-auto max-w-full px-3 my-auto">
               <div className="h-full">
-                <h4 className="flex text-lg font-bold place-items-center gap-3 text-center">
+                <h4 className="flex text-3xl font-bold place-items-center gap-5 text-center text-white">
                   {employee.first_name} {employee.last_name}
                   <div
                     className={cn(
-                      "text-xs rounded-full py-1 px-2 border font-normal flex place-items-center gap-1 cursor-pointer",
+                      "text-xs rounded-2xl py-1 px-2 border font-normal flex place-items-center gap-1 cursor-pointer",
                       employee.status === "Available"
-                        ? "text-green-500 bg-green-500 bg-opacity-20 border-green-500"
+                        ? "text-white-500 bg-slate-500 bg-opacity-70 border-gray-800"
                         : employee.status === "In Progress"
                         ? "text-yellow-300 bg-yellow-300 bg-opacity-20 border-yellow-300"
                         : "text-red-500 bg-red-500 bg-opacity-20 border-red-500"
@@ -222,7 +223,7 @@ export default function Page({ employee, roles }: any) {
                     {employee.status}
                   </div>
                 </h4>
-                <p className="mb-0 font-semibold leading-normal text-sm">
+                <p className="mb-0 font-semibold leading-normal text-md text-white">
                   {employee.roles.role}
                 </p>
               </div>
@@ -234,116 +235,151 @@ export default function Page({ employee, roles }: any) {
             </div>
           </div>
         </div>
-        <div className="w-full pb-6 mx-auto removable mt-4">
+        <div className="w-full mx-auto removable">
           <div className="flex flex-wrap -mx-3 drop-zone">
-            <div className="w-full max-w-full px-3 lg-max:mt-6 xl:w-4/12 mb-4 draggable">
-              <div className="relative flex flex-col h-full min-w-0 break-words bg-white rounded-2xl">
-                <div className="p-4 pb-0 mb-0 bg-white border-b-0 rounded-2xl">
-                  <div className="flex flex-wrap -mx-3">
-                    <div className="flex items-center w-full max-w-full px-3 shrink-0 md:w-8/12 md:flex-none">
-                      <h6>Profile Information</h6>
+            <div className="w-full max-w-full px-3 lg-max:mt-6 xl:w-4/12 mb-0 draggable">
+              <div className="relative flex flex-col h-full min-w-0 break-words rounded-2xl shadow-lg">
+                <div className="p-0 pb-0 mb-0 bg-gradient-to-r from-blue-300 to-blue-400 border-b-0 rounded-2xl">
+                  <div className="flex flex-wrap -mx-3 px-4 py-2">
+                    <div className="flex items-center w-full max-w-full px-3 shrink-0 md:w-8/12 md:flex-none font-bold py-1 text-white">
+                      <h6>Location</h6>
                     </div>
                   </div>
-                  <div className="flex-auto p-4">
-                    <p className="leading-normal text-sm">Hi im bunda rhama</p>
-                    <hr className="h-px my-6 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
-
+                  <div className="flex-auto px-4 pb-4">
+                    <iframe
+                      className="w-full min-h-[300px] rounded-xl"
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.644211999011!2d123.2626467!3d9.289425199999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zOsKwMjMnMzQuMCJTIDEyM8KwMzMnMTAuNyJF!5e0!3m2!1sen!2sph!4v1648767287790!5m2!1sen!2sph"
+                      allowFullScreen={true}
+                      loading="lazy"
+                    ></iframe>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full max-w-full px-3 lg-max:mt-6 xl:w-4/12 mb-0 draggable">
+              <div className="relative flex flex-col h-full min-w-0 break-words bg-gradient-to-r from-blue-400 via-blue-400 to-blue-400 rounded-2xl shadow-lg">
+                <div className="p-0 pb-0 mb-0 bg-gradient-to-r from-blue-400 via-blue-400 to-blue-400 border-b-0 rounded-2xl">
+                  <div className="flex flex-wrap -mx-3 px-4 py-2">
+                    <div className="flex items-center w-full max-w-full px-3 shrink-0 md:w-8/12 md:flex-none font-bold py-1 text-white">
+                      <h6>General Information</h6>
+                    </div>
+                  </div>
+                  <div className="flex-auto px-4 pb-4">
                     <ul className="flex flex-col pl-0 mb-0 rounded-lg">
-                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-sm text-inherit">
-                        <strong className="text-slate-700">Full Name:</strong>{" "}
-                        Bunda rhana
+                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal  border-0 rounded-t-lg text-sm text-inherit">
+                        <div className="w-full flex flex-col gap-2">
+                          <span className="text-xs 2xl:text-md font-semibold text-white flex justify-center place-items-center w-fit gap-1">
+                            Full Name
+                          </span>
+                          <div className="w-full min-w-0 bg-blue-300 rounded-lg">
+                            <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                              {employee.first_name} {employee.last_name}
+                            </p>
+                          </div>
+                        </div>
                       </li>
 
-                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-sm text-inherit">
-                        <strong className="text-slate-700">
-                          Phone Number:
-                        </strong>{" "}
-                        09659074552
+                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normale border-0 rounded-t-lg text-sm text-inherit">
+                        <div className="w-full flex flex-col gap-2">
+                          <span className="text-xs 2xl:text-md font-semibold text-white flex justify-center place-items-center w-fit gap-1">
+                            Sex
+                          </span>
+                          <div className="w-full min-w-0 bg-blue-300 rounded-lg">
+                            <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                              {employee.gender}
+                            </p>
+                          </div>
+                        </div>
                       </li>
 
-                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-sm text-inherit">
-                        <strong className="text-slate-700">Email:</strong>{" "}
-                        Bunda_rhana@gmail.com
+                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normale border-0 rounded-t-lg text-sm text-inherit">
+                        <div className="w-full flex flex-col gap-2">
+                          <span className="text-xs 2xl:text-md font-semibold text-white flex justify-center place-items-center w-fit gap-1">
+                            Birthday
+                          </span>
+                          <div className="w-full min-w-0 bg-blue-300 rounded-lg">
+                            <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                              {employee.dob}
+                            </p>
+                          </div>
+                        </div>
                       </li>
-                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-sm text-inherit">
-                        <strong className="text-slate-700">Address:</strong>{" "}
-                        Talay, Pangasinan
+                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normale border-0 rounded-t-lg text-sm text-inherit">
+                        <div className="w-full flex flex-col gap-2">
+                          <span className="text-xs 2xl:text-md font-semibold text-white flex justify-center place-items-center w-fit gap-1">
+                            Address
+                          </span>
+                          <div className="w-full min-w-0 bg-blue-300 rounded-lg">
+                            <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                              {employee.address}
+                            </p>
+                          </div>
+                        </div>
                       </li>
                     </ul>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="w-full max-w-full px-3 lg-max:mt-6 xl:w-4/12 mb-4 draggable">
-              <div className="relative flex flex-col h-full min-w-0 break-words bg-white rounded-2xl">
-                <div className="p-4 pb-0 mb-0 bg-white border-b-0 rounded-2xl">
-                  <div className="flex flex-wrap -mx-3">
-                    <div className="flex items-center w-full max-w-full px-3 shrink-0 md:w-8/12 md:flex-none">
-                      <h6>Profile Information</h6>
+            <div className="w-full max-w-full px-3 lg-max:mt-6 xl:w-4/12 mb-0 draggable">
+              <div className="relative flex flex-col h-full min-w-0 break-words bg-gradient-to-r from-blue-400 via-blue-400 to-blue-400 rounded-2xl shadow-lg">
+                <div className="p-0 pb-0 mb-0 bg-gradient-to-r from-blue-400 via-blue-400 to-blue-400 border-b-0 rounded-2xl">
+                  <div className="flex flex-wrap -mx-3 px-4 py-2">
+                    <div className="flex items-center w-full max-w-full px-3 shrink-0 md:w-8/12 md:flex-none font-bold py-1 text-white">
+                      <h6>Employee Information</h6>
                     </div>
                   </div>
-                  <div className="flex-auto p-4">
-                    <p className="leading-normal text-sm">Hi im bunda rhama</p>
-                    <hr className="h-px my-6 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
-
+                  <div className="flex-auto px-4 pb-4">
                     <ul className="flex flex-col pl-0 mb-0 rounded-lg">
-                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-sm text-inherit">
-                        <strong className="text-slate-700">Full Name:</strong>{" "}
-                        Bunda rhana
+                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal  border-0 rounded-t-lg text-sm text-inherit">
+                        <div className="w-full flex flex-col gap-2">
+                          <span className="text-xs 2xl:text-md font-semibold text-white flex justify-center place-items-center w-fit gap-1">
+                            Job title
+                          </span>
+                          <div className="w-full min-w-0 bg-blue-300 rounded-lg">
+                            <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                              {employee.roles.role}
+                            </p>
+                          </div>
+                        </div>
                       </li>
 
-                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-sm text-inherit">
-                        <strong className="text-slate-700">
-                          Phone Number:
-                        </strong>{" "}
-                        09659074552
+                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normale border-0 rounded-t-lg text-sm text-inherit">
+                        <div className="w-full flex flex-col gap-2">
+                          <span className="text-xs 2xl:text-md font-semibold text-white flex justify-center place-items-center w-fit gap-1">
+                            Email
+                          </span>
+                          <div className="w-full min-w-0 bg-blue-300 rounded-lg">
+                            <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                              {employee.email}
+                            </p>
+                          </div>
+                        </div>
                       </li>
 
-                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-sm text-inherit">
-                        <strong className="text-slate-700">Email:</strong>{" "}
-                        Bunda_rhana@gmail.com
+                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normale border-0 rounded-t-lg text-sm text-inherit">
+                        <div className="w-full flex flex-col gap-2">
+                          <span className="text-xs 2xl:text-md font-semibold text-white flex justify-center place-items-center w-fit gap-1">
+                            Mobile Number
+                          </span>
+                          <div className="w-full min-w-0 bg-blue-300 rounded-lg">
+                            <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                              {employee.contact_number}
+                            </p>
+                          </div>
+                        </div>
                       </li>
-                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-sm text-inherit">
-                        <strong className="text-slate-700">Address:</strong>{" "}
-                        Talay, Pangasinan
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full max-w-full px-3 lg-max:mt-6 xl:w-4/12 mb-4 draggable">
-              <div className="relative flex flex-col h-full min-w-0 break-words bg-white rounded-2xl">
-                <div className="p-4 pb-0 mb-0 bg-white border-b-0 rounded-2xl">
-                  <div className="flex flex-wrap -mx-3">
-                    <div className="flex items-center w-full max-w-full px-3 shrink-0 md:w-8/12 md:flex-none">
-                      <h6>Profile Information</h6>
-                    </div>
-                  </div>
-                  <div className="flex-auto p-4">
-                    <p className="leading-normal text-sm">Hi im bunda rhama</p>
-                    <hr className="h-px my-6 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
-
-                    <ul className="flex flex-col pl-0 mb-0 rounded-lg">
-                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-sm text-inherit">
-                        <strong className="text-slate-700">Full Name:</strong>{" "}
-                        Bunda rhana
-                      </li>
-
-                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-sm text-inherit">
-                        <strong className="text-slate-700">
-                          Phone Number:
-                        </strong>{" "}
-                        09659074552
-                      </li>
-
-                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-sm text-inherit">
-                        <strong className="text-slate-700">Email:</strong>{" "}
-                        Bunda_rhana@gmail.com
-                      </li>
-                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normal bg-white border-0 rounded-t-lg text-sm text-inherit">
-                        <strong className="text-slate-700">Address:</strong>{" "}
-                        Talay, Pangasinan
+                      <li className="relative block px-4 py-2 pt-0 pl-0 leading-normale border-0 rounded-t-lg text-sm text-inherit">
+                        <div className="w-full flex flex-col gap-2">
+                          <span className="text-xs 2xl:text-md font-semibold text-white flex justify-center place-items-center w-fit gap-1">
+                            Added at
+                          </span>
+                          <div className="w-full min-w-0 bg-blue-300 rounded-lg">
+                            <p className="text-sm 2xl:text-md text-black gap-2 p-2 max-w-[260px] truncate">
+                              {employee.created_at}
+                            </p>
+                          </div>
+                        </div>
                       </li>
                     </ul>
                   </div>
