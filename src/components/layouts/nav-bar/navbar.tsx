03d07f69d20d83940d4a5f3ffb1bdcca5a14bf12
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-04-07 12:57:45 +0800

    Did some things

--- src/components/layouts/nav-bar/navbar.tsx
+++ src/components/layouts/nav-bar/navbar.tsx
@@ -6,7 +6,7 @@ import { NavigationMenuDemo } from "./Navigation-menu";
 
 export default function Navbar() {
   return (
-    <div className="flex w-full h-fit justify-center place-items-center border-b items-center px-4 py-4 bg-white sticky top-0 z-[20]">
+    <div className="flex w-full h-fit justify-center place-items-center  items-center px-4 py-4 bg-white sticky top-0 z-[20]">
       <div className="w-[90%] flex justify-between place-items-center">
         <MainNav className="mx-6" />
         <div className="flex items-center gap-4">
