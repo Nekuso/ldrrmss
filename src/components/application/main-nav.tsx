c136ecd352b7257eb51de4d0754b70c188a6a433
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-04-09 17:52:01 +0800

    sige

--- src/components/application/main-nav.tsx
+++ src/components/application/main-nav.tsx
@@ -24,7 +24,7 @@ export function MainNav({
           key={link.name}
           href={link.href}
           passHref
-          className="text-sm font-medium relative hover:text-black hover:bg-white rounded-md px-4 py-2.5"
+          className="text-sm font-medium relative hover:text-black hover:bg-white bg-opacity-90 rounded-md px-4 py-2.5"
         >
           <span className="relative">{link.name}</span>
         </Link>
