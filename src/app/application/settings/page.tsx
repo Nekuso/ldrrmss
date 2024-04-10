fc015c4630a9b9eb93299c0d1b339eec667f7e7e
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-04-10 23:57:27 +0800

    asdad

--- src/app/application/settings/page.tsx
+++ src/app/application/settings/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 export default function Page() {
   return (
     <div className="flex flex-col justify-start place-items-center w-full h-full gap-7 p-8">
-      <div className="space-y-2 w-[90%] h-fit bg-white p-4 rounded-2xl">
+      <div className="space-y-2 w-[90%] h-fit bg-white bg-opacity-85 p-4 rounded-2xl">
         Hi!
       </div>
     </div>
--- src/components/layouts/nav-bar/Navigation-menu.tsx
+++ src/components/layouts/nav-bar/Navigation-menu.tsx
@@ -14,44 +14,6 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
-
 export function NavigationMenuDemo() {
   return (
     <NavigationMenu>
@@ -118,30 +80,16 @@ export function NavigationMenuDemo() {
             Components
           </NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
-              ))}
+            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <ListItem
+                href="../../application/announcement"
+                title="Announcements"
+              >
+                Rescue announcements to people
+              </ListItem>
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
-        {/* <Dead-end>*/}
-        <NavigationMenuItem>
-          <Link href="../../application/announcement" legacyBehavior passHref>
-            <NavigationMenuLink
-              className={`bg-transparent ${navigationMenuTriggerStyle()}`}
-            >
-              Announcement
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-
         {/* <Dead-end>*/}
         <NavigationMenuItem>
           <Link href="../../application/settings" legacyBehavior passHref>
