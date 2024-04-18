9adec4b5e63dcb6b4e94630a0ff347a28e7b382e
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-04-19 00:56:57 +0800

    enough for tnoght

--- src/components/layouts/landing-navbar/landing_navbar.tsx
+++ src/components/layouts/landing-navbar/landing_navbar.tsx
@@ -3,7 +3,7 @@ import { LandingNavbarContents } from "./landing_navbar_contents";
 export default function Navbar() {
   return (
     <div className="flex w-full h-fit justify-center place-items-center items-center px-5 py-4 bg-gray-900 text-white sticky top-0 z-[20]">
-      <div className="w-[90%] flex justify-between place-items-center">
+      <div className="w-full flex justify-between place-items-center">
         <LandingNavbarContents />
       </div>
     </div>
--- src/components/layouts/landing-navbar/landing_navbar_contents.tsx
+++ src/components/layouts/landing-navbar/landing_navbar_contents.tsx
@@ -1,139 +1,41 @@
-"use client";
-
-import * as React from "react";
-import Link from "next/link";
-
-import { cn } from "@/lib/utils";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
-
 export function LandingNavbarContents() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="bg-transparent">
-            Operations
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              {/* <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      shadcn/ui
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components built with Radix UI and
-                      Tailwind CSS.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li> */}
-              <ListItem href="../../application/requests" title="Requests">
-                Rescue related requests
-              </ListItem>
-              <ListItem href="../../application/employees" title="Employees">
-                Rescue employees
-              </ListItem>
-              <ListItem href="../../application/workers" title="Workers">
-                Rescue employees who handles on dispatch
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        {/* <Dead-end>*/}
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="bg-transparent">
-            Inventory
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <ListItem
-                href="../../application/inventory/equipments"
-                title="Equiments"
-              >
-                Rescue related equipments
-              </ListItem>
-              <ListItem
-                href="../../application/inventory/food_supply"
-                title="Food Supply"
-              >
-                Rescue rations related to people
-              </ListItem>
-              <ListItem
-                href="../../application/inventory/vehicles"
-                title="Vehicles"
-              >
-                Rescue Transport Vehicles
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        {/* <Dead-end>*/}
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="bg-transparent">
-            Components
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <ListItem
-                href="../../application/announcement"
-                title="Announcements"
-              >
-                Rescue announcements to people
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        {/* <Dead-end>*/}
-        <NavigationMenuItem>
-          <Link href="../../application/settings" legacyBehavior passHref>
-            <NavigationMenuLink
-              className={`bg-transparent ${navigationMenuTriggerStyle()}`}
+    <nav className="bg-opacity-75 backdrop-blur-lg fixed w-full top-0 z-10 p-8">
+      <div className="max-w-screen-2xl mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <div className="mr-2">
+            <a
+              href="/"
+              className="text-white text-3xl font-bold hover:text-blue-600"
             >
-              Settings
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        {/* </Dead-end> */}
-      </NavigationMenuList>
-    </NavigationMenu>
+              Your Logo
+            </a>
+          </div>
+          <div className="flex items-center space-x-4 ">
+            <a
+              href="/"
+              className="text-white hover:text-blue-600 transition duration-300 text-xl font-medium relative inline-block "
+            >
+              Home
+              <span className="absolute top-0 left-0 w-full h-full bg-white opacity-0 transition-opacity duration-300 hover:opacity-50 rounded-md"></span>
+            </a>
+            <a
+              href="#"
+              className="text-white hover:text-blue-600 transition duration-300 text-xl font-medium relative inline-block"
+            >
+              About
+              <span className="absolute top-0 left-0 w-full h-full bg-white opacity-0 transition-opacity duration-300 hover:opacity-50 rounded-md"></span>
+            </a>
+            <a
+              href="#"
+              className="text-white hover:text-blue-600 transition duration-300 text-xl font-medium relative inline-block"
+            >
+              Contact
+              <span className="absolute top-0 left-0 w-full h-full bg-white opacity-0 transition-opacity duration-300 hover:opacity-50 rounded-md"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
-
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
-ListItem.displayName = "ListItem";
