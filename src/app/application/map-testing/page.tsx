c3e1437ca3980be20f5af480527796ec37537c6e
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-04-22 11:29:00 +0800

    sige lang ay

--- src/app/application/map-testing/page.tsx
+++ src/app/application/map-testing/page.tsx
@@ -1,29 +1,18 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-
-interface MapProps {
-  center: [number, number];
-  zoom: number;
-  style: React.CSSProperties;
-}
 
 export default function Page() {
-  const position: [number, number] = [9.4196, 123.1845];
-  const zoom = 12;
-
-  const mapProps: MapProps = {
-    center: position,
-    zoom: zoom,
-    style: { width: "100%", height: "100%" },
-  };
-
   return (
     <div className="flex flex-col justify-start place-items-center w-full h-full gap-7 p-8">
       <div className="space-y-2 w-[90%] h-[80vh] bg-white bg-opacity-85 p-4 rounded-2xl">
-        <MapContainer {...mapProps}>
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        </MapContainer>
+        <iframe
+          width="100%"
+          height="100%"
+          src={`https://www.openstreetmap.org/export/embed.html?bbox=123.1615,9.381,123.208,9.4448&layer=mapnik`}
+        ></iframe>
+      </div>
+      <div>
+        <input type="text" placeholder="Search for a location" />
+        <button>Search</button>
       </div>
     </div>
   );
