8ab87be89c35b82e14c342be9bcfa48c4a7c2149
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-05-20 21:34:43 +0800

    dadads

--- src/app/application/requests/transactions-table/orders-table/add-order/LocationSearch.tsx
+++ src/app/application/requests/transactions-table/orders-table/add-order/LocationSearch.tsx
@@ -1,19 +1,37 @@
 // LocationSearch.tsx
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useMap } from "react-leaflet";
 import { Controller } from "react-hook-form";
-import { TileLayer, MapContainer } from "react-leaflet";
+import { TileLayer, MapContainer, Marker } from "react-leaflet";
+import { icon, Icon } from "leaflet";
 import { Input } from "@/components/ui/input";
-// LocationSearch.tsx
 
 export const LocationSearch = ({ control }: { control: any }) => {
+  const [markerPosition, setMarkerPosition] = useState<[number, number] | null>(
+    null
+  );
+
+  const customMarkerIcon: Icon = icon({
+    iconUrl: "path-to-your-red-icon.png", // replace with the path to your red icon
+    iconSize: [38, 95], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  });
+
   return (
     <>
+      <label
+        htmlFor="location_search"
+        className="block text-sm font-medium text-gray-700 mb-2" // Add margin-bottom
+      >
+        Location Search
+      </label>
       <Controller
         name="location_search"
         control={control}
         render={({ field }) => (
           <Input
+            id="location_search"
             className="rounded-lg border-slate-600/50 w-3/4 mx-auto my-2" // Adjust width to 75% and add margin
             {...field}
             type="text"
@@ -31,6 +49,9 @@ export const LocationSearch = ({ control }: { control: any }) => {
         style={{ height: "400px", width: "100%" }}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {markerPosition && (
+          <Marker position={markerPosition} icon={customMarkerIcon} />
+        )}
         <Controller
           name="location_search"
           control={control}
@@ -46,7 +67,12 @@ export const LocationSearch = ({ control }: { control: any }) => {
                   const searchResults = await response.json();
                   if (searchResults && searchResults[0]) {
                     const { lat, lon } = searchResults[0];
-                    map.flyTo([parseFloat(lat), parseFloat(lon)], 13);
+                    const newPosition: [number, number] | null = [
+                      parseFloat(lat),
+                      parseFloat(lon),
+                    ];
+                    map.flyTo(newPosition, 13);
+                    setMarkerPosition(newPosition);
                   }
                 }
               };
