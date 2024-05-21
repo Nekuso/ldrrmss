7e2ef4c5dff233595b03943ed686419eb0a256c6
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-05-21 13:17:39 +0800

    Revert "Mapping polish"
    This reverts commit 8539e8c8dc5b3b6067e2f7b0d6fa70e740038892.

--- src/app/application/requests/transactions-table/orders-table/add-order/LocationSearch.tsx
+++ src/app/application/requests/transactions-table/orders-table/add-order/LocationSearch.tsx
@@ -21,23 +21,6 @@ export const LocationSearch = ({ control }: { control: any }) => {
     popupAnchor: [-3, -76],
   });
 
-  const fetchRoute = async (from: [number, number], to: [number, number]) => {
-    const response = await fetch(
-      `https://api.openrouteservice.org/v2/directions/driving-car?api_key=<your_api_key>&start=${from[1]},${from[0]}&end=${to[1]},${to[0]}&instructions=true`
-    );
-    const data = await response.json();
-    if (data.features && data.features[0] && data.features[0].geometry) {
-      const route = data.features[0].geometry.coordinates.map(
-        ([lon, lat]: [number, number]) => [lon, lat]
-      );
-      const instructions = data.features[0].properties.segments[0].steps.map(
-        (step: any) => step.instruction
-      );
-      return { route, instructions };
-    }
-    return null;
-  };
-
   const fetchLocation = async (location: string) => {
     const response = await fetch(
       `https://nominatim.openstreetmap.org/search?format=json&q=${location}`
